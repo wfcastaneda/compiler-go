44fb9513c0c3aa3cfe5c86ffc369b061970d7ae9
Will C <willcastaneda.me@gmail.com>
2022-10-31 20:58:07 -0700

    Begin evaluator implementation

--- interpreter-go/repl/repl.go
+++ interpreter-go/repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"gopherlang-interpreter/evaluator"
 	"gopherlang-interpreter/lexer"
 	"gopherlang-interpreter/parser"
 	"io"
@@ -30,8 +31,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
