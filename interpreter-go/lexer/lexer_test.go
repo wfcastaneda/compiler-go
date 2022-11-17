450ca9e875894df991f9ab67e4f9a268ed82d2ba
Will C <willcastaneda.me@gmail.com>
2022-11-16 20:10:45 -0800

    Add builtin support. Add .

--- interpreter-go/lexer/lexer_test.go
+++ interpreter-go/lexer/lexer_test.go
@@ -27,6 +27,8 @@ if (5 < 10) {
 10 != 9;
 "foobar"
 "foo bar"
+[1, 2];
+{"foo": "bar"}
 `
 
     tests := []struct {
@@ -108,7 +110,18 @@ if (5 < 10) {
 		{token.SEMICOLON, ";"},
 		{token.STRING, "foobar"},
 		{token.STRING, "foo bar"},
-        {token.EOF, ""},
+		{token.LBRACKET, "["},
+		{token.INT, "1"},
+		{token.COMMA, ","},
+		{token.INT, "2"},
+		{token.RBRACKET, "]"},
+		{token.SEMICOLON, ";"},
+		{token.LBRACE, "{"},
+		{token.STRING, "foo"},
+		{token.COLON, ":"},
+		{token.STRING, "bar"},
+		{token.RBRACE, "}"},
+		{token.EOF, ""},
     }
 
     l := New(input)
